9aeee11d22abf96c6c0e055b3052fda3e345fc98
Leslie Mathys <dev@lesliemathys.com>
2022-07-10 23:09:18 +1000

    Write logic for player vs computer outcomes

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ function computerPlay() {
 }
 computerPlay()
 
-function playRound() {
+function playRound(playerSelection, computerPlay) {
     const playerWin = 'You won!';
     const compWin = 'You lost!';
     const draw = 'It\'s a tie!';
+
+    if (playerSelection === 'rock' && computerPlay === 'rock') {
+        console.log(draw);
+    } else if (playerSelection === 'rock' && computerPlay === 'paper') {
+        console.log(compWin);
+    } else if (playerSelection === 'rock' && computerPlay === 'scissors') {
+        console.log(playerWin);
+    } else if (playerSelection === 'paper' && computerPlay === 'rock') {
+        console.log(playerWin);
+    } else if (playerSelection === 'paper' && computerPlay === 'paper') {
+        console.log(draw);
+    } else if (playerSelection === 'paper' && computerPlay === 'scissors') {
+        console.log(compWin);
+    } else if (playerSelection === 'scissors' && computerPlay === 'rock') {
+        console.log(compWin);
+    } else if (playerSelection === 'scissors' && computerPlay === 'paper') {
+        console.log(playerWin);
+    } else if (playerSelection === 'scissors' && computerPlay === 'scissors') {
+        console.log(draw);
+    }
 }
 playRound()
