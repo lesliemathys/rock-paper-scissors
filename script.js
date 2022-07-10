c778d50d58f570af7048e8d64f28e4b12e021ed6
Leslie Mathys <dev@lesliemathys.com>
2022-07-10 22:27:59 +1000

    Write computerPlay function

---
+++ script.js
@@ -0,0 +1,8 @@
+function computerPlay() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    console.log(randomChoice);
+}
+
+computerPlay()
