4cd519a1adfac5b05b3793b1eec9264326752ea4
Leslie Mathys <dev@lesliemathys.com>
2022-07-10 22:32:04 +1000

    Revert "Write computerPlay function"
    This reverts commit c778d50d58f570af7048e8d64f28e4b12e021ed6.

--- script.js
+++
@@ -1,8 +0,0 @@
-function computerPlay() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-
-    console.log(randomChoice);
-}
-
-computerPlay()
