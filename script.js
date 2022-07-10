b0429af4169a0cd566fd56c3dd7e11dccb6a2bda
Leslie Mathys <dev@lesliemathys.com>
2022-07-10 22:58:28 +1000

    Add outcome variables

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-
     return randomChoice;
 }
 computerPlay()
 
 function playRound() {
-
+    const playerWin = 'You won!';
+    const compWin = 'You lost!';
+    const draw = 'It\'s a tie!';
 }
+playRound()
